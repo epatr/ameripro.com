eee7c561b628ae2e25ec6b11f0d02f8294ef05ff
Eric Patrick <erstpa@gmail.com>
2020-03-23 16:24:07 -0400

    Linked directly to services

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Index() {
     return (
       <div>
         {/* <Link href="/about"><a>About Page</a></Link> */}
-        <Link href="https://ameripropest.com"><a target="_blank">AmeriPro Pest Control</a></Link><br />
-        <Link href="https://chooseameripro.com"><a target="_blank">AmeriPro Inspection Corporation</a></Link>
+        <Link href="https://ameripropest.com/environmental-services/"><a target="_blank">AmeriPro Pest Control</a></Link><br />
+        <Link href="https://chooseameripro.com/services/environmental-services/"><a target="_blank">AmeriPro Inspection Corporation</a></Link>
       </div>
     );
   }
