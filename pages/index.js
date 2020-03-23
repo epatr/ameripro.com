181e915776ed07e5a155e52f0f0699a1020d5cfb
Eric Patrick <erstpa@gmail.com>
2020-03-23 11:51:09 -0400

    Added default pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Link from 'next/link';
+
 export default function Index() {
     return (
       <div>
+          <Link href="/about"><a>About Page</a></Link>
         <p>Hello Next.js</p>
       </div>
     );
