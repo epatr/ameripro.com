bc17035e8ec8f98b382584e609a13fde03e48887
Eric Patrick <erstpa@gmail.com>
2020-03-23 12:29:08 -0400

    Added linebreak

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Index() {
     return (
       <div>
         {/* <Link href="/about"><a>About Page</a></Link> */}
-        <Link href="https://ameripropest.com"><a target="_blank">AmeriPro Pest Control</a></Link>
+        <Link href="https://ameripropest.com"><a target="_blank">AmeriPro Pest Control</a></Link><br />
         <Link href="https://chooseameripro.com"><a target="_blank">AmeriPro Inspection Corporation</a></Link>
       </div>
     );
